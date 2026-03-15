1ed320e28d91404f248c67dcaf25bfc89a1af439
Xulan541 <3360748455@qq.com>
2026-03-15 16:41:55 +0800

    Site updated: 2026-03-15 16:41:55

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1773563978868";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1773564111199";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
